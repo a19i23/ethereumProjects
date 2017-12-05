804ed73f5c350ed4a1b3b172dbb50b8d489d54db
Alan Villa <a19i23villa@gmail.com>
2017-12-04 19:10:12 -0600

    adding dragonstone example contract

---
+++ dragonstone.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.0;
+
+contract DragonStone {
+    address public creator;
+    mapping (address => uint) public balances;
+
+    //event that notifies when a transfer has completed
+    event Delivered(address from, address to, uint amount);
+
+    function DragonStone() {
+        creator = msg.sender;
+    }
+
+    function create(address receiver, uint amount) {
+        if (msg.sender != creator) throw;
+        balances[receiver] += amount;
+    }
+
+    function transfer(address receiver, uint amount) {
+        if(balances[msg.sender] < amount) throw;
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        Delivered(msg.sender, receiver, amount);
+    }
+
+}
